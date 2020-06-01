1cff81d2b474fb412c2b3e68a5c21f68555a81ff
“abshirjama” <Abshir05@gmail.com>
2020-06-01 08:50:32 -0500

    added two more services

--- services/authentication/src/index.ts
+++ services/authentication/src/index.ts
@@ -4,6 +4,9 @@ import cors from 'cors'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import Mongoose from 'mongoose'
+
 
 
 
@@ -15,6 +18,7 @@ app.use(cors())
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+app.use(errorHandler)
 
 app.get('/auth', (req: Request, res: Response) => {
     res.send('auth: i am alive')
